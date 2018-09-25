4ba94d10d34f6e154e363ba8d9d9251ab2638f25
quoctv1202 <quoctv1202@gmail.com>
2018-09-25 11:09:38 +0700

    Hello action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello Sample App"
+  end
+  def bye
+    render html: "Good bye"
+  end
 end
